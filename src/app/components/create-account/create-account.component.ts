7ff4798eb82f9a05aae243463c4a619ad770681d
souhail rhaq <souhail@example.com>
2026-01-23 13:47:57 +0100

    create-account

--- src/app/components/create-account/create-account.component.ts
+++ src/app/components/create-account/create-account.component.ts
@@ -10,27 +10,30 @@ import { FireBaseService } from '../../services/fire-base.service';
   styleUrl: './create-account.component.scss',
 })
 export class CreateAccountComponent {
-  public fixedPathTranslate: string = 'NEXT_GAME.CREATE_ACCOUNT.';
-  public mail: string = '';
-  public password: string = '';
-  public confirmPassword: string = '';
-  public errorMessage: string = '';
-  public isloading: boolean = false;
-  public passwordEye: boolean = false;
-  public confirmPasswordEye: boolean = false;
-  public firstName: string = '';
-  public lastName: string = '';
+  public readonly fixedPathTranslate = 'NEXT_GAME.CREATE_ACCOUNT.';
+
+  public mail = '';
+  public password = '';
+  public confirmPassword = '';
+  public firstName = '';
+  public lastName = '';
+
+  public errorMessage = '';
+  public isloading = false;
+
+  public passwordEye = false;
+  public confirmPasswordEye = false;
 
   constructor(
     private readonly headerService: HeaderService,
     private readonly fireBaseService: FireBaseService
   ) {}
 
-  ngOnInit(): void {
+  public ngOnInit(): void {
     this.headerService.selectPage(7);
   }
 
-  private initVal() {
+  private initVal(): void {
     this.mail = '';
     this.password = '';
     this.confirmPassword = '';
@@ -38,39 +41,54 @@ export class CreateAccountComponent {
   }
 
   private checkForamtMail(mail: string): boolean {
-    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    const emailRegex: RegExp = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return emailRegex.test(mail);
   }
 
-  public createAccount() {
-    if (
+  private missingRequiredFields(): boolean {
+    return (
       !this.mail ||
       !this.password ||
       !this.confirmPassword ||
       !this.firstName ||
       !this.lastName
-    ) {
+    );
+  }
+
+  public createAccount(): void {
+    // validation
+    if (this.missingRequiredFields()) {
       this.errorMessage = this.fixedPathTranslate + 'ERRORS.ERROR_1';
       return;
-    } else if (this.checkForamtMail(this.mail)) {
+    }
+
+    if (this.checkForamtMail(this.mail)) {
       this.errorMessage = this.fixedPathTranslate + 'ERRORS.ERROR_3';
       return;
-    } else if (this.password.length < 8) {
+    }
+
+    if (this.password.length < 8) {
       this.errorMessage = this.fixedPathTranslate + 'ERRORS.ERROR_2';
       return;
-    } else if (this.password !== this.confirmPassword) {
+    }
+
+    if (this.password !== this.confirmPassword) {
       this.errorMessage = this.fixedPathTranslate + 'ERRORS.ERROR_4';
       return;
-    } else {
-      this.fireBaseService
-        .createUserWithEmailAndPassword(this.mail, this.password)
-        .then((res) => {
-          console.log(res);
-        })
-        .catch((err) => {
-          console.log(err);
-        });
     }
+
+    // action
     this.isloading = true;
+
+    this.fireBaseService
+      .createUserWithEmailAndPassword(this.mail, this.password)
+      .then((res) => {
+        console.log(res);
+        // si tu veux garder la méthode, tu peux la laisser ici sans changer la logique globale
+        // this.initVal();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
   }
 }
